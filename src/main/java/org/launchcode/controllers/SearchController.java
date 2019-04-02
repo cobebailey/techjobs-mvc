63816fc7d82092acacf402613c2f8bc12de070e8
Cobe Bailey <baileycobe@yahoo.com>
2019-04-01 19:03:50 -0500

    final commit all done fool

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -16,6 +16,19 @@ import java.util.HashMap;
 @RequestMapping("search")
 public class SearchController {
 
+
+    static HashMap<String, String> columnChoices = new HashMap<>();
+
+    public SearchController () {
+        columnChoices.put("core competency", "Skill");
+        columnChoices.put("employer", "Employer");
+        columnChoices.put("location", "Location");
+        columnChoices.put("position type", "Position Type");
+        columnChoices.put("all", "All");
+    }
+
+
+
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
@@ -24,4 +37,25 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
+
+
+    @RequestMapping(value = "results")
+    public String listSearchValues(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        model.addAttribute("columns", ListController.columnChoices);
+
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> items = JobData.findByValue(searchTerm);
+            model.addAttribute("title", "All Jobs");
+            model.addAttribute("items", items);
+            return "search";
+        } else {
+            ArrayList<HashMap<String, String>> items = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "All " + columnChoices.get(searchType) + " Values");
+            model.addAttribute("searchType", searchType);
+            model.addAttribute("items", items);
+            return "search";
+        }
+
+    }
 }
